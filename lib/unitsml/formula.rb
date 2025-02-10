d79cbf6136b4e3102427563bf39aa6d5a35c604a
suleman-uzair <suleman@workytical.com>
2025-02-10 16:09:43 +0500

    minor: error message changes

--- lib/unitsml/formula.rb
+++ lib/unitsml/formula.rb
@@ -172,7 +172,7 @@ module Unitsml
         [unitsml] OR
         [unitsml] run following command to install the gem:
         [unitsml] gem install plurimath
-        [unitsml] If this is a bug, please report the formula at our issue tracker at:
+        [unitsml] If this is a bug, please report at our official issue tracker:
         [unitsml] https://github.com/unitsml/unitsml-ruby/issues
       MESSAGE
 
