d31862020fb4264f12b4fbc3789e550a0bbfa17f
suleman-uzair <suleman@workytical.com>
2025-03-07 10:49:43 +0500

    Fixed multiplier specified keywords output

--- lib/unitsml/extender.rb
+++ lib/unitsml/extender.rb
@@ -14,37 +14,43 @@ module Unitsml
     end
 
     def to_mathml(options)
-      multiplier = case options[:multiplier]
-                   when :space
-                     rspace = "thickmathspace"
-                     "&#x2062;"
-                   when :nospace
-                     "&#x2062;"
-                   else
-                     extender = options[:multiplier] || "⋅"
-                     Utility.string_to_html_entity(extender)
-                   end
+      extender = multiplier(options[:multiplier] || "⋅", unicode: true)
+      rspace = "thickmathspace" if options[:multiplier] == :space
       {
         method_name: :mo,
-        value: ::Mml::Mo.new(value: multiplier, rspace: rspace),
+        value: ::Mml::Mo.new(value: extender, rspace: rspace),
       }
     end
 
     def to_latex(options)
-      options[:multiplier] || "/"
+      multiplier(options[:multiplier] || "/")
     end
 
     def to_asciimath(options)
-      options[:multiplier] || symbol
+      multiplier(options[:multiplier] || symbol)
     end
 
     def to_html(options)
-      options[:multiplier] || "&#x22c5;"
+      multiplier(options[:multiplier] || "⋅", unicode: true)
     end
 
     def to_unicode(options)
-      options[:multiplier] ||
-        symbol == "*" ? "·" : symbol
+      extender = options[:multiplier] ||
+                   symbol == "*" ? "·" : symbol
+      multiplier(extender)
+    end
+
+    private
+
+    def multiplier(extender, unicode: false)
+      case extender
+      when :space
+        unicode ? "&#x2062;" : " "
+      when :nospace
+        unicode ? "&#x2062;" : ""
+      else
+        unicode ? Utility.string_to_html_entity(extender) : extender
+      end
     end
   end
 end
